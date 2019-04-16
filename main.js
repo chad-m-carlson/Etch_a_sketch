90d09a7ff53ffbdba82f06a83bf09d6cc243feaf
Chad Carlson <chad.m.carlson@gmail.com>
2019-04-15 20:48:05 -0600

    minor js fixes

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
 var grid = prompt('Enter grid size. (Bigger number means smaller sections', '20');
-console.log(grid);
 createArea(grid);
 const reset = document.getElementById('reset');
 const colors = document.getElementById('colors');
 let area = grid * grid;
 
-
 reset.addEventListener('click', e => {
-  for (let i = 0; i < area; i++){
+  for (let i = 0; i < area; i++) {
     const workingDivs = document.querySelectorAll('#workingDivs')
     workingDivs.forEach(element => {
-      element.remove()
+      element.remove();
     });
   }
   var grid = prompt('Enter grid size. (Bigger number means smaller sections)', '20');
   createArea(grid);
-  console.log(grid);
 });
 
 colors.addEventListener('change', e => {
@@ -51,13 +48,11 @@ function createArea(grid) {
     playArea.appendChild(addDiv);
   }
 }
-
 document.addEventListener('mouseover', e => {
   if (e.target.classList.contains('workingDivs')) {
     e.target.style.backgroundColor = 'black';
   }
 });
-
 document.addEventListener('click', e => {
   if (e.target.classList.contains('workingDivs')) {
     e.target.style.backgroundColor = '#b2ada7';
